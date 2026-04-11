3635550ec549ce6083784c9854ef88db59e46623
Sagar Khandagre <sagar.khandagre998@gmail.com>
2026-04-11 21:06:00 +0530

    test: remove cypress patientStudyView/Edit tests and page objects

--- frontend/cypress/pages/HomePage.js
+++ frontend/cypress/pages/HomePage.js
@@ -1,5 +1,4 @@
 import LoginPage from "./LoginPage";
-import PatientStudyEditPage from "./PatientStudyEditPage";
 import PatientEntryPage from "./PatientEntryPage";
 import PatientMergePage from "./PatientMergePage";
 import OrderEntityPage from "./OrderEntityPage";
@@ -15,7 +14,6 @@ import StudyReportPage from "./StudyReportPage";
 import DashBoardPage from "./DashBoard";
 import AdminPage from "./AdminPage";
 import HelpPage from "./HelpPage";
-import PatientStudyViewPage from "./PatientStudyViewPage";
 
 class HomePage {
   constructor() {
@@ -27,8 +25,6 @@ class HomePage {
       patientMenu: "span#menu_patient",
       patientAddEdit: "#menu_patient_add_or_edit_nav",
       patientMerge: "#menu_patient_merge",
-      patientStudyView: "#menu_patient_consult_react_nav",
-      patientStudyEdit: "#menu_patient_study_edit_react_nav",
       sampleEditNav: "#menu_sample_edit_nav",
       workplanMenu: "span#menu_workplan",
       workplanTestNav: "#menu_workplan_test_nav",
@@ -129,26 +125,6 @@ class HomePage {
     cy.get(this.selectors.patientMenu).click();
     cy.get(this.selectors.patientAddEdit).click();
     return new PatientEntryPage();
-  }
-
-  // Patient Study View
-  goToPatientStudyView() {
-    this.openNavigationMenu();
-    cy.get(this.selectors.patientMenu).click();
-    cy.get(this.selectors.patientStudyView)
-      .should("exist")
-      .click({ force: true });
-    return new PatientStudyViewPage();
-  }
-
-  // Patient Study Edit
-  goToPatientStudyEdit() {
-    this.openNavigationMenu();
-    cy.get(this.selectors.patientMenu).click();
-    cy.get(this.selectors.patientStudyEdit)
-      .should("exist")
-      .click({ force: true });
-    return new PatientStudyEditPage();
   }
 
   // Patient Merge (Admin function)
